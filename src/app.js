f2a0bc45e5674b52754221ab2a7829001db1c1a1
aadityasharma-05 <adityasharma0052005@gmail.com>
2026-06-14 09:31:55 +0530

    bug solve

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ const app = express()
 
 const allowedOrigins = [
     process.env.FRONTEND_URL || "http://localhost:5173",
-    "http://localhost:5173",
-    "http://localhost:3000",
 ]
 
 app.use(cors({
